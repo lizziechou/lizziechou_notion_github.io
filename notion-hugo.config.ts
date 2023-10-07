93c222843e0cc5ef9ed02e3645aac57542415939
lizzie <74082582+lizziechou@users.noreply.github.com>
2023-10-07 09:49:53 +0800

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://lizziechou.notion.site/Notion-DoIt-1e28af36e1f048a7a5a5882618a94158?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
